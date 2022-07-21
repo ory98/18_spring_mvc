e424554436446a530573f415db5cc8f209ceb642
User <User@F12>
2022-07-21 17:48:50 +0900

    7/21 수업

---
+++ 18_spring_mvc/src/main/java/com/spring/mvc/dynamicQuery/dao/DynamicQueryDao.java
@@ -0,0 +1,25 @@
+package com.spring.mvc.dynamicQuery.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class DynamicQueryDao {
+	
+	@Autowired
+	private SqlSession sqlSession;
+	
+	public List<Map<String, Object>> selectOrderList() { // DB에서 꺼내와 
+		return sqlSession.selectList("dynamicQuery.list"); // controller에게 return해줌 
+	}
+	
+	public List<Map<String, Object>> ifEx(Map<String, String> searchMap) { // DB에서 꺼내와 
+		return sqlSession.selectList("dynamicQuery.ifEx"); // controller에게 return해줌 
+	}
+	
+	
+}
